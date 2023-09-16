ee26b021fa9014600d680db838840529e87b6997
oyakovenko <alex231@ukr.net>
2023-09-16 11:31:22 +0300

    handle errors add new feature

--- todo_list_project/gui.py
+++ todo_list_project/gui.py
@@ -1,6 +1,8 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+clock = sg.Text('', key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to do", key="todo")
 add_button = sg.Button("Add")
@@ -10,36 +12,47 @@ edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
-window = sg.Window('My To-Do APP', layout=[[label],
+window = sg.Window('My To-Do APP', layout=[[clock],
+                                           [label],
                                            [input_box, add_button],
                                            [list_box, edit_button, complete_button],
                                            [exit_button]],
                                             font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(values)
     if event == 'Add':
         todos = functions.get_todos()
+        if values["todo"] == "":
+            sg.Popup('Please, enter the value!', font=('Helvetica', 20))
+            continue
         new_todo = values["todo"] + "\n"
         todos.append(new_todo)
         functions.write_todos(todos)
         window['todos'].update(values=todos)
     elif event == 'Edit':
-        todo_to_edit = values['todos'][0]
-        new_todo = values['todo'] + "\n"
-        todos = functions.get_todos()
-        ind = todos.index(todo_to_edit)
-        todos[ind] = new_todo
-        functions.write_todos(todos)
-        window['todos'].update(values=todos)
+        try:
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            ind = todos.index(todo_to_edit)
+            todos[ind] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        except IndexError:
+            sg.Popup('Please, choose the value!', font=('Helvetica', 20))
     elif event == 'Complete':
-        todo_to_remove = values['todos'][0]
-        todos = functions.get_todos()
-        todos.remove(todo_to_remove)
-        functions.write_todos(todos)
-        window['todos'].update(values=todos)
-        window['todo'].update(value='')
+        try:
+            todo_to_remove = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_remove)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        except IndexError:
+            sg.Popup('Please, choose the value!', font=('Helvetica', 20))
     elif event == 'Exit':
         break
     elif event == 'todos':
