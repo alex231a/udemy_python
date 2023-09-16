1c88229338c5112d22a2b126bc5ba6dba446e6f6
oyakovenko <alex231@ukr.net>
2023-09-16 21:32:58 +0300

    TODO WEB add new items on the page

--- todo_list_project/web.py
+++ todo_list_project/web.py
@@ -1,11 +1,22 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo_in = st.session_state["new_todo"] + "\n"
+    todos.append(todo_in)
+    functions.write_todos(todos)
+
+
 st.title("MY TODO APP")
 
-todos = functions.get_todos()
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Enter new todo...")
+st.text_input(label="", placeholder="Enter new todo...", on_change=add_todo, key="new_todo")
+
+print("hello")
+st.session_state
 
